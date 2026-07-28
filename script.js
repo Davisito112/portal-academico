594fa00efafd8731a3fddd6b3f267d74c44ec3ae
Davisito112 <134255757+Davisito112@users.noreply.github.com>
2026-07-28 13:54:10 -0400

    Actualizar script.js

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function entrar() {
 let usuario = document.getElementById("usuario").value;
 let clave = document.getElementById("clave").value;
 
-if (usuario === "765324" && clave === "David123") {
+if(usuario === "765324" && clave === "David123"){
 
 document.getElementById("login").style.display = "none";
 document.getElementById("panel").style.display = "block";
 
-} else {
+}else{
 
 alert("Usuario o contraseña incorrectos");
 
 }
 
-}
+} 
